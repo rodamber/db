764b96f8a970aa4bbd820713890439d1acfe7049
rodamber <rodrigoambernardo@gmail.com>
2016-12-01 16:36:59 +0000

    adicionei o schema dos profs (com on delete cascade)

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,108 +1,104 @@
--- Duvidas:
--- 1. not null
--- 2. tipo do telefone
--- 3. muita atencao auto_increment
--- 4. tipo da foto
--- 5. Ver o que fazer on delete e on update
+drop table if exists estado;
+drop table if exists paga;
+drop table if exists aluga;
+drop table if exists reserva;
+drop table if exists oferta;
+drop table if exists posto;
+drop table if exists espaco;
+drop table if exists fiscaliza;
+drop table if exists arrenda;
+drop table if exists alugavel;
+drop table if exists edificio;
+drop table if exists fiscal;
+drop table if exists user;
 
-CREATE TABLE IF NOT EXISTS User (
-    nif INT NOT NULL,
-    nome VARCHAR(255) NOT NULL,
-    telefone INT NOT NULL,
-    PRIMARY KEY (nif)
-);
 
-CREATE TABLE IF NOT EXISTS Fiscal (
-       id INT NOT NULL,
-       empresa VARCHAR(255) NOT NULL,
-       PRIMARY KEY (id)
-);
+create table user (
+    nif varchar(9) not null unique,
+    nome varchar(80) not null,
+    telefone varchar(26) not null,
+    primary key(nif));
 
-CREATE TABLE IF NOT EXISTS Edificio (
-       morada VARCHAR(255) NOT NULL,
-       PRIMARY KEY (morada)
-);
+create table fiscal (
+    id int not null unique,
+    empresa varchar(255) not null,
+    primary key(id));
 
-CREATE TABLE IF NOT EXISTS Alugavel (
-       morada VARCHAR(255) NOT NULL,
-       codigo INT NOT NULL AUTO_INCREMENT,
-       foto VARCHAR(255) NOT NULL,
-       PRIMARY KEY (morada, codigo),
-       FOREIGN KEY (morada) REFERENCES Edificio (morada) ON DELETE CASCADE
-);
+create table edificio (
+    morada varchar(255) not null unique,
+    primary key(morada));
 
-CREATE TABLE IF NOT EXISTS Arrenda (
-       morada VARCHAR(255) NOT NULL,
-       codigo INT NOT NULL,
-       nif INT NOT NULL AUTO_INCREMENT,
-       PRIMARY KEY (morada, codigo),
-       FOREIGN KEY (morada, codigo) REFERENCES Alugavel (morada, codigo) ON DELETE CASCADE
-);
+create table alugavel (
+    morada varchar(255) not null,
+    codigo varchar(255) not null,
+    foto varchar(255) not null,
+    primary key(morada, codigo),
+    foreign key(morada) references edificio(morada)) on delete cascade;
 
-CREATE TABLE IF NOT EXISTS Fiscaliza (
-       id INT NOT NULL,
-       morada VARCHAR(255) NOT NULL,
-       codigo INT NOT NULL,
-       PRIMARY KEY (id, morada, codigo),
-       FOREIGN KEY (id) REFERENCES Fiscal (id),
-       FOREIGN KEY (morada, codigo) REFERENCES Arrenda (morada, codigo) ON DELETE CASCADE
-);
+create table arrenda (
+    morada varchar(255) not null,
+    codigo varchar(255) not null,
+    nif varchar(9) not null,
+    primary key(morada, codigo),
+    foreign key(morada, codigo) references alugavel(morada, codigo) on delete cascade,
+    foreign key(nif) references user(nif)) on delete cascade;
 
-CREATE TABLE IF NOT EXISTS Espaco (
-       morada VARCHAR(255) NOT NULL,
-       codigo INT NOT NULL, -- auto_increment,
-       PRIMARY KEY (morada, codigo),
-       FOREIGN KEY (morada, codigo) REFERENCES Alugavel (morada, codigo) ON DELETE CASCADE
-);
+create table fiscaliza (
+    id int not null,
+    morada varchar(255) not null ,
+    codigo varchar(255) not null ,
+    primary key(id, morada, codigo),
+    foreign key(morada, codigo) references arrenda(morada, codigo) on delete cascade,
+    foreign key(id) references fiscal(id)) on delete cascade;
 
-CREATE TABLE IF NOT EXISTS Posto (
-       morada VARCHAR(255) NOT NULL,
-       codigo INT NOT NULL, -- auto_increment,
-       codigo_espaco INT NOT NULL,
-       PRIMARY KEY (morada, codigo),
-       FOREIGN KEY (morada, codigo) REFERENCES Alugavel (morada, codigo) ON DELETE CASCADE,
-       FOREIGN KEY (morada, codigo_espaco) REFERENCES Espaco (morada, codigo) ON DELETE CASCADE
-);
+create table espaco (
+    morada varchar(255) not null,
+    codigo varchar(255) not null,
+    primary key(morada, codigo),
+    foreign key(morada, codigo) references alugavel(morada, codigo)) on delete cascade;
 
-CREATE TABLE IF NOT EXISTS Oferta (
-       morada VARCHAR(255) NOT NULL,
-       codigo INT NOT NULL, -- auto_increment,
-       data_inicio TIMESTAMP NOT NULL,
-       data_fim TIMESTAMP NOT NULL,
-       tarifa INT NOT NULL,
-       PRIMARY KEY (morada, codigo, data_inicio),
-       FOREIGN KEY (morada, codigo) REFERENCES Alugavel (morada, codigo) ON DELETE CASCADE
-);
+create table posto (
+    morada varchar(255) not null,
+    codigo varchar(255) not null,
+    codigo_espaco varchar(255) not null,
+    primary key(morada, codigo),
+    foreign key(morada, codigo) references alugavel(morada, codigo) on delete cascade,
+    foreign key(morada, codigo_espaco) references espaco(morada, codigo)) on delete cascade;
 
-CREATE TABLE IF NOT EXISTS Aluga (
-       morada VARCHAR(255) NOT NULL,
-       codigo INT NOT NULL, -- auto_increment,
-       data_inicio TIMESTAMP NOT NULL,
-       nif INT NOT NULL,
-       numero INT NOT NULL AUTO_INCREMENT,
-       PRIMARY KEY (morada, codigo, data_inicio, nif, numero),
-       FOREIGN KEY (morada, codigo, data_inicio) REFERENCES Oferta (morada, codigo, data_inicio),
-       FOREIGN KEY (nif) REFERENCES User (nif),
-       FOREIGN KEY (numero) REFERENCES Reserva (numero)
-);
+create table oferta (
+    morada varchar(255) not null,
+    codigo varchar(255) not null,
+    data_inicio date not null,
+    data_fim date not null,
+    tarifa numeric(19,4) not null,
+    primary key(morada, codigo, data_inicio),
+    foreign key(morada, codigo) references alugavel(morada, codigo)) on delete cascade;
 
-CREATE TABLE IF NOT EXISTS Paga (
-       numero INT NOT NULL AUTO_INCREMENT,
-       data TIMESTAMP NOT NULL,
-       metodo VARCHAR(255) NOT NULL,
-       PRIMARY KEY (numero),
-       FOREIGN KEY (numero) REFERENCES Reserva (numero)
-);
+create table reserva (
+    numero varchar(255) not null unique,
+    primary key(numero));
 
-CREATE TABLE IF NOT EXISTS Estado (
-       numero INT NOT NULL AUTO_INCREMENT,
-       timestamp TIMESTAMP NOT NULL,
-       estado VARCHAR(255) NOT NULL,
-       PRIMARY KEY (numero),
-       FOREIGN KEY (numero) REFERENCES Reserva (numero)
-);
+create table aluga (
+    morada varchar(255) not null,
+    codigo varchar(255) not null,
+    data_inicio date not null,
+    nif varchar(9) not null,
+    numero varchar(255) not null,
+    primary key(morada, codigo, data_inicio, nif, numero),
+    foreign key(morada, codigo, data_inicio) references oferta(morada, codigo, data_inicio) on delete cascade,
+    foreign key(nif) references user(nif) on delete cascade,
+    foreign key(numero) references reserva(numero)) on delete cascade;
 
-CREATE TABLE IF NOT EXISTS Reserva (
-       numero INT NOT NULL AUTO_INCREMENT,
-       PRIMARY KEY (numero)
-);
+create table paga (
+    numero varchar(255) not null unique,
+    data timestamp not null,
+    metodo varchar(255) not null,
+    primary key(numero),
+    foreign key(numero) references reserva(numero)) on delete cascade;
+
+create table estado (
+    numero varchar(255) not null,
+    time_stamp timestamp not null,
+    estado varchar(255) not null,
+    primary key(numero, time_stamp),
+    foreign key(numero) references reserva(numero)) on delete cascade;
