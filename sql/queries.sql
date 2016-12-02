3d80f365b8ccaaa2295b4c9baad58c91c28fd53a
rodamber <rodrigoambernardo@gmail.com>
2016-12-02 00:00:07 +0000

    Added query (d)

--- sql/queries.sql
+++ sql/queries.sql
@@ -11,15 +11,14 @@ select distinct morada
 from aluga
 group by morada
 having count(numero) > (
-	select avg(C)
-	from (
-		select count(numero) as C
-		from aluga
-		group by morada
-	) as counts
-);
+  select avg(C)
+  from (
+    select count(numero) as C
+    from aluga
+    group by morada) as counts);
 
--- c) Quais utilizadores cujos alugáveis foram fiscalizados sempre pelo mesmo fiscal?
+-- c) Quais utilizadores cujos alugáveis foram fiscalizados sempre pelo mesmo
+-- fiscal?
 
 select distinct A.nif
 from aluga A, fiscaliza F
@@ -27,10 +26,38 @@ where A.morada = F.morada and A.codigo = F.codigo
 group by A.nif
 having count(distinct F.id) = 1;
 
--- d) Qual o montante total realizado (pago) por cada espaço durante o ano de 2016?
--- Assuma que a tarifa indicada na oferta é diária. Deve considerar os casos em que o
--- espaço foi alugado totalmente ou por postos.
+-- d) Qual o montante total realizado (pago) por cada espaço durante o ano de
+-- 2016? Assuma que a tarifa indicada na oferta é diária. Deve considerar os
+-- casos em que o espaço foi alugado totalmente ou por postos.
 
--- e) Quais os espaços de trabalho cujos postos nele contidos foram todos alugados? (Por
--- alugado entende-se um posto de trabalho que tenha pelo menos uma oferta aceite,
--- independentemente das suas datas.)
+select morada, codigo, sum(montante)
+from (
+   -- Qual o montante realizado por cada espaco alugado no seu todo em 2016?
+   select E.morada, E.codigo,
+          -- Temos que ter em conta as datas de fim que acabam para alem de 2016
+          -- e as datas de inicio antes de 2016.
+          sum(O.tarifa * datediff(least(O.data_fim, '2016/12/12'),
+                                  greatest(O.data_inicio, '2016/01/01'))) as montante
+   from espaco E, oferta O, aluga A
+   where E.morada = O.morada and O.morada = A.morada
+     and E.codigo = O.codigo and O.codigo = A.codigo
+   group by E.morada, E.codigo
+
+   union
+
+   -- Qual o montante realizado por cada espaco em alugueres de postos
+   -- individuais em 2016?
+   select E.morada, E.codigo,
+          -- Temos que ter em conta as datas de fim que acabam para alem de 2016
+          -- e as datas de inicio antes de 2016.
+          sum(O.tarifa * datediff(least(O.data_fim, '2016/12/12'),
+          greatest(O.data_inicio, '2016/01/01'))) as montante
+   from espaco E, posto P, oferta O, aluga A
+   where E.morada = P.morada and P.morada = O.morada and O.morada = A.morada
+     and E.codigo = P.codigo_espaco and P.codigo = O.codigo and O.codigo = A.codigo
+   group by E.morada, E.codigo) as montantes
+group by morada, codigo;
+
+-- e) Quais os espaços de trabalho cujos postos nele contidos foram todos
+-- alugados? (Por alugado entende-se um posto de trabalho que tenha pelo menos
+-- uma oferta aceite, independentemente das suas datas.)
