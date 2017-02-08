e070946d1cccbeb5fda7e28e441b54be5cad8074
rodamber <rodrigoambernardo@gmail.com>
2017-02-08 18:36:13 +0000

    updated olap.sql

--- sql/olap.sql
+++ sql/olap.sql
@@ -8,6 +8,8 @@ FROM
     d_data    AS DT
 WHERE
     FR.localid = DL.localid AND
-    FR.dataid  = DT.dataid
+    FR.dataid  = DT.dataid  AND
+    FR.montante_pago <> 0
 GROUP BY
     DL.localid, DT.dataid WITH ROLLUP;
+    WITH ROLLUP;
